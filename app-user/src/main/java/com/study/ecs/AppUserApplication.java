a232ef92fd434273981fa222adf382056223a8fe
juhyeon1114 <juhyeon1114@gmail.com>
2025-12-22 22:57:49 +0900

    Dockerfile 작성

--- app-user/src/main/java/com/study/ecs/AppUserApplication.java
+++ app-user/src/main/java/com/study/ecs/AppUserApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AppUserApplication {
 
-	public static void main(String[] args) {
+	static void main(String[] args) {
 		SpringApplication.run(AppUserApplication.class, args);
 	}
 
