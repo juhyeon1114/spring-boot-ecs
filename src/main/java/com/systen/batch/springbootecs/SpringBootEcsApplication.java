e72aa1202e0a9951aad67450b31986e8fb515b3c
juhyeon1114 <juhyeon1114@gmail.com>
2025-12-22 22:26:33 +0900

    init

---
+++ src/main/java/com/systen/batch/springbootecs/SpringBootEcsApplication.java
@@ -0,0 +1,13 @@
+package com.systen.batch.springbootecs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEcsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEcsApplication.class, args);
+	}
+
+}
