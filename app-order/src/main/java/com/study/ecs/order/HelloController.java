334cc54abe1d340b699a38b35477df4b0580bf07
juhyeon1114 <juhyeon1114@gmail.com>
2025-12-22 23:28:57 +0900

    fix

--- app-order/src/main/java/com/study/ecs/order/HelloController.java
+++ app-order/src/main/java/com/study/ecs/order/HelloController.java
@@ -1,4 +1,4 @@
-package com.study.ecs;
+package com.study.ecs.order;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
--- app-user/src/main/java/com/study/ecs/user/AppUserApplication.java
+++ app-user/src/main/java/com/study/ecs/user/AppUserApplication.java
@@ -1,4 +1,4 @@
-package com.study.ecs;
+package com.study.ecs.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
